54e70b8fae78e9f7d5fdc00f1bd42b46def7f862
Andrew <andersen7298@gmail.com>
2020-10-04 21:05:16 +0300

    Реализована маршрутизация

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import Main from '../main/main';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import AddReview from '../add-review/add-review';
+import Film from '../film/film';
+import Main from '../main/main';
+import MyList from '../my-list/my-list';
+import Player from '../player/player';
+import SignIn from '../sign-in/sign-in';
 
 const App = (props) => {
   const {title, genre, year} = props;
 
   return (
-    <Main title={title} genre={genre} year={year}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <Main title={title} genre={genre} year={year} />
+        </Route>
+        <Route exact path='/login'>
+          <SignIn />
+        </Route>
+        <Route exact path='/mylist'>
+          <MyList />
+        </Route>
+        <Route exact path='/film/:id'>
+          <Film />
+        </Route>
+        <Route exact path='/film/:id/review'>
+          <AddReview />
+        </Route>
+        <Route exact path='/player/:id'>
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
