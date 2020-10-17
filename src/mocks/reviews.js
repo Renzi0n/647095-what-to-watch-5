b1245595511c957b591865108a3225c6f3259b41
Andrew <andersen7298@gmail.com>
2020-10-17 18:40:13 +0300

    Созданы моки

---
+++ src/mocks/reviews.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    text: `Discerning travellers and Wes Anderson
+      fans will luxuriate in the glorious Mittel-European
+      kitsch of one of the director's funniest and most
+      exquisitely designed movies in years.`,
+    name: `Kate Muir`,
+    date: `December 24, 2020`,
+    rate: 8.9
+  },
+  {
+    text: `The mannered, madcap
+      proceedings are often delightful, occasionally silly,
+      and here and there, gruesome and/or heartbreaking.`,
+    name: `Matthew Lickona`,
+    date: `December 20, 2020`,
+    rate: 5.9
+  },
+  {
+    text: `I didn't find it amusing, and
+      while I can appreciate the creativity, it's an
+      hour and 40 minutes I wish I could take back.`,
+    name: `Amanda Greever`,
+    date: `November 18, 2020`,
+    rate: 7.1
+  },
+  {
+    text: `It is certainly a magical and childlike way of storytelling,
+      even if the content is a little more adult..`,
+    name: `Paula Fleri-Soler`,
+    date: `December 15, 2020`,
+    rate: 8.2
+  },
+  {
+    text: `Anderson's films are too precious for some, but for those of us willing
+      to lose ourselves in them, they're a delight.
+      "The Grand Budapest Hotel" is no different, except that he has added
+      a hint of gravitas to the mix, improving the recipe.`,
+    name: `Bill Goodykoontz`,
+    date: `December 23, 2020`,
+    rate: 5
+  },
+  {
+    text: `The mannered, madcap
+      proceedings are often delightful, occasionally silly,
+      and here and there, gruesome and/or heartbreaking.`,
+    name: `Matthew Lickona`,
+    date: `December 18, 2020`,
+    rate: 5.9
+  },
+  {
+    text: `I didn't find it amusing, and
+      while I can appreciate the creativity, it's an
+      hour and 40 minutes I wish I could take back.`,
+    name: `Amanda Greever`,
+    date: `November 12, 2020`,
+    rate: 5.6
+  },
+  {
+    text: `Discerning travellers and Wes Anderson
+    fans will luxuriate in the glorious Mittel-European
+    kitsch of one of the director's funniest and most
+    exquisitely designed movies in years.`,
+    name: `Kate Muir`,
+    date: `December 21, 2020`,
+    rate: 6.9
+  },
+];
